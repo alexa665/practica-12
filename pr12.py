e9a88893b9fffbb7d111308da30d0c9372baa1f2
alexa665 <a.23308051281205@cbtis128.edu.mx>
2024-09-24 20:50:06 -0600

    Add files via upload

---
+++ pr12.py
@@ -0,0 +1,10 @@
+print(" ")
+print("alexa guadalupe ramirez manzo")
+print(" ")
+x=int(input("ingresa el valor")) #variable x para ingresar un valor
+y=int(input("ingresa el valor")) #variable y para ingresar un valor
+z=int(input("ingresa el valor")) #variable z para ingresar un valor
+print(" ")
+promedio = (x+y+z)/3 #suma y division para dar el promedio
+print("el resultado del promedio es",promedio) #inpresion para que de el promedio
+print(" ")
